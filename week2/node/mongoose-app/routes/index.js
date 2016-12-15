44322cc9d10617bbc237fc8a97e4f8651b6288c1
jfhinchcliffe <hinchy@gmail.com>
2016-12-15 17:30:30 +1100

    Finally got that Node CRUD going

--- week2/node/mongoose-app/routes/index.js
+++ week2/node/mongoose-app/routes/index.js
@@ -29,7 +29,16 @@ router.post('/', (req, res) => {
   });
 });
 
-router.get('/show/:id', (req, res) => {
+// router.get('/show/:id', (req, res) => {
+//   Ingredient.findById(req.params.id)
+//     .then(ingredient => {
+//       res.render('show', { 
+//         ingredient: ingredient  
+//        }); 
+//     });
+// });
+
+router.get('/:id', (req, res) => {
   Ingredient.findById(req.params.id)
     .then(ingredient => {
       res.render('show', { 
@@ -47,16 +56,60 @@ router.get('/update/:id', (req, res) => {
     });
 });
 
-router.post('/update/:id', (req, res) => {
-  const name = req.body.ingredient_name;
+// router.post('/update/:id', (req, res) => {
+//   const name = req.body.ingredient_name;
+//   Ingredient.findById(req.params.id)
+//     .then(ingredient => {
+//       ingredient.name = name;
+//       ingredient.save()
+//         .then(() => {
+//           res.redirect('/show/' + req.params.id);
+//       });
+//   });
+// });
+
+
+function show(res, id) {
   Ingredient.findById(req.params.id)
-    .then(ingredient => {
-      ingredient.name = name;
-      ingredient.save()
-        .then(() => {
-          res.redirect('/show/' + req.params.id);
-      });
+      .then(ingredient => {
+        ingredient.name = name;
+        ingredient.save()
+          .then(() => {
+            res.redirect(req.params.id);
+        });
+    });
+}
+
+function del(res, id) {
+  Ingredient.findOneAndRemove({ _id: id }, (err, ingredient) => {
+    if (err) {
+      console.log("NOT BALEETED");
+    } else {
+      console.log("BALEETED");
+    }
   });
+  res.redirect('/');
+}
+
+router.post('/:id', (req, res) => {
+  const id = req.params.id;
+  const name = req.body.ingredient_name;
+  const method = req.body._method;
+  console.log("ID: " + req.body.ingredient_name);
+  if (method === 'DELETE'){
+    del(res, id);
+  } else {
+    //show(res, id);
+
+    Ingredient.findById(req.params.id)
+      .then(ingredient => {
+        ingredient.name = name;
+        ingredient.save()
+          .then(() => {
+            res.redirect(req.params.id);
+        });
+    });
+  }
 });
 
 
